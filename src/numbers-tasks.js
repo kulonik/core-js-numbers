f155692a9455c6ad9d30659155df4b5603d78eb1
kulonik <annakhmv@gmail.com>
2024-11-04 12:43:04 +0500

    feat: complete getSumOfNumbers task

--- src/numbers-tasks.js
+++ src/numbers-tasks.js
@@ -576,8 +576,10 @@ function getIntegerPartNumber(number) {
  * 1, 2, 3       => 6
  * 0.1, 0.2, 0.3 => 0.6
  */
-function getSumOfNumbers(/* x1, x2, x3 */) {
-  throw new Error('Not implemented');
+function getSumOfNumbers(x1, x2, x3) {
+  const arr = [x1, x2, x3];
+  const sum = arr.reduce((partialSum, a) => partialSum + a, 0);
+  return sum.toFixed(2);
 }
 
 /**
